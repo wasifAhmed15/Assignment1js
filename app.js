536488cc8ae054f705d1b2e3bc8bb7d5cd836830
wasifAhmed15 <wasifahmedkhan99@gmail.com>
2023-05-04 21:26:11 +0500

    compelete work all

---
+++ app.js
@@ -0,0 +1,128 @@
+// alert("HELLO WORLD")
+
+
+// Variables for Strings cheapter:1
+// var firstNamw= "Wasif"
+
+
+
+// Variables for Numbers Cheapter:3
+
+// var number=56
+
+// var firstNumber=67;
+// var secondNumber=23
+// var addNumber=firstNumber+secondNumber
+// console.log(addNumber)
+
+
+// Variable Names Legal and Illegal chapter:4
+
+// var 1="wasif" Illegal 
+
+// var a="wasif"  Legal
+
+// var fir73="wasif" Legal
+// var #dj="wasif" Illegal
+
+// var First_name="wasif" Legal
+
+
+// Math expressions:
+// Familiar operators chapter:5
+
+// var firstNumber=67;
+// var secondNumber=23
+// var addNumber=firstNumber+secondNumber
+// console.log(addNumber)
+
+// Math expressions:
+// Unfamiliar operators chapter:6
+
+// var a = 10
+// var a=a+1
+// console.log(a)
+
+// var a=10
+// console.log(
+//  ++a   
+// )
+
+// var a=10
+// a++
+// console.log(a)
+
+// var a=10;
+// a--
+// console.log(a)
+
+
+// var a=10;
+//  console.log(--a)
+// 
+//  var a=11;
+//  console.log(a++ + ++a + a + --a + a--)
+
+// Math expressions:
+// Eliminating ambiguity chapter:7
+
+// var a = 1+4-8*6
+// console.log(a)
+
+// var a = (1+4)-5
+// console.log(a)
+
+// var a =  (2 * 4) * 4 + 2;
+// console.log(a)
+
+ 
+// Concatenating text strings chapter:8 
+
+// var userName="wasif"
+// alert("thanks"+userName)
+
+// var userName="wasif"
+// alert("thanks"+" "+userName)
+
+
+// Prompts chapter:9
+
+
+// var a = prompt("Your Name","First Name")
+// console.log(a)
+
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer)
+// console.log(spec)
+
+
+// var names =prompt("Name")
+// var fatherName=prompt("Father Name")
+// var phoneNumber=prompt("Phone Number")
+// console.log(names+fatherName+phoneNumber)
+
+// if statements chapter:10
+// var names = "wasif"
+// if (names=="wasif") {
+//     alert("match")
+// }
+// else("not match")
+
+
+// a=12;
+// if (a==10) {
+//     console.log(a)
+// }
+// else(
+//     console.log("false")
+// )
+
+
+
+ 
+
+
+
+
